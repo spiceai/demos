f974aa771f4875f977ff9a052e2791692275f970
Evgenii Khramkov <evgenii@spice.ai>
2024-06-06 14:49:04 +0900

    update switcher

--- content-aware-ai/app/app/@slides/Switcher.tsx
+++ content-aware-ai/app/app/@slides/Switcher.tsx
@@ -4,9 +4,12 @@ import { Button } from "@/components/ui/button";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
+const maxState = 3;
+
 export const Switcher = () => {
   const router = useRouter();
   const params = useSearchParams();
+  const state = parseInt(params.get("state") || "0");
 
   useEffect(() => {
     if (!params.has("state")) {
@@ -17,9 +20,8 @@ export const Switcher = () => {
   }, []);
 
   const toState = (d: number) => {
-    const state = parseInt(params.get("state") || "0");
     const next = state + d;
-    if (next < 0 || next > 3) {
+    if (next < 0 || next > maxState) {
       return;
     }
     const newParams = new URLSearchParams(params);
@@ -29,10 +31,21 @@ export const Switcher = () => {
 
   return (
     <div className="absolute top-4 right-4 flex items-center gap-1">
-      <Button variant="ghost" size="sm" onClick={() => toState(-1)}>
+      <Button
+        disabled={state <= 0}
+        variant="ghost"
+        size="sm"
+        onClick={() => toState(-1)}
+      >
         prev
       </Button>
-      <Button variant="ghost" size="sm" onClick={() => toState(1)}>
+
+      <Button
+        disabled={state >= maxState}
+        variant="ghost"
+        size="sm"
+        onClick={() => toState(1)}
+      >
         next
       </Button>
     </div>
